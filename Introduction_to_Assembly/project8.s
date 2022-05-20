3815934e3e6d867e0ebdab0ad63bb9627191a013
Rachit Garg <rachitgarg900@gmail.com>
2022-05-20 09:30:05 +0200

    Update project8.s

--- Introduction_to_Assembly/project8.s
+++ Introduction_to_Assembly/project8.s
@@ -1,1 +1,11 @@
-TODO : Conditional Execution example
+.global _start
+_start:
+
+	mov r0, #3
+	mov r1, #2
+	
+	@ There are other such instructions like addge, addeq, subeq, etc.
+	cmp r0, r1
+	movge r2, #10 @ move 10 to r2 only if r0 >= r1
+	movlt r2, #5 @ opposite of above
+	@ Saves making a separate branch for simple instructions
