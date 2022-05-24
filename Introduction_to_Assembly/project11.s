77fb0021c89969a113d7755639fa7fcf6ce70577
Rachit Garg <rachitgarg900@gmail.com>
2022-05-24 11:19:51 +0200

    Update project11.s

--- Introduction_to_Assembly/project11.s
+++ Introduction_to_Assembly/project11.s
@@ -1,1 +1,11 @@
+.equ SWITCHES, 0xff200040 @ Address of switches in memory
+.equ LEDS, 0xff200000 @ Address of LEDs in memory
 
+.global _start
+_start:
+	
+	ldr r0, =SWITCHES
+	ldr r1, =LEDS
+	
+	ldr r2, [r0] @ Read switches
+	str r2, [r1] @ Write this value to LEDs output
